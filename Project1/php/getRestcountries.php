783bacefd4a24ebb2068ebb3d9b6ec4baac21400
OzgunVedat <145506970+OzgunVedat@users.noreply.github.com>
2024-04-04 02:05:06 +0100

    Add files via upload

---
+++ Project1/php/getRestcountries.php
@@ -0,0 +1,26 @@
+<?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$url = 'https://restcountries.com/v2/alpha/' . $_REQUEST['countryCode'];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url);
+
+$result=curl_exec($ch);
+
+curl_close($ch);
+
+$decode = json_decode($result,true);
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['data'] = $decode;
+
+
+echo json_encode($output);
+
+?>
